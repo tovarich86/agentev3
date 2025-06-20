d5f50474bc11b528c33fb5503f46cf2f6e085207
Danilo da Cruz Ferraz <93060216+tovarich86@users.noreply.github.com>
2025-06-20 13:00:37 -0300

    Create catalog_data.py

---
+++ catalog_data.py
@@ -0,0 +1,410 @@
+company_catalog_rich = [
+    {
+        "canonical_name": "2W ECOBANK S.A",
+        "aliases": ["2w ecobank", "2w energia"]
+    },
+    {
+        "canonical_name": "AERIS IND. E COM. DE EQUIP. GERACAO DE ENERGIA S.A",
+        "aliases": ["aeris", "aeris energia"]
+    },
+    {
+        "canonical_name": "ALIANSCE SONAE SHOPPING CENTERS S.A",
+        "aliases": ["aliansce sonae", "aliansce"]
+    },
+    {
+        "canonical_name": "ALLOS S.A",
+        "aliases": ["allos", "aliansce sonae br malls"] # Allos é a fusão da Aliansce Sonae com a BR Malls
+    },
+    {
+        "canonical_name": "ALPARGATAS S.A",
+        "aliases": ["alpargatas", "havaianas", "ousa", "dupe"] # Marcas famosas são aliases cruciais
+    },
+    {
+        "canonical_name": "ALPER CONSULTORIA E CORRETORA DE SEGUROS S.A",
+        "aliases": ["alper", "alper seguros"]
+    },
+    {
+        "canonical_name": "AMBEV S.A",
+        "aliases": ["ambev"]
+    },
+    {
+        "canonical_name": "AMERICANAS S.A",
+        "aliases": ["americanas", "lojas americanas"]
+    },
+    {
+        "canonical_name": "ANIMA HOLDING S.A",
+        "aliases": ["anima", "anima educacao"]
+    },
+    {
+        "canonical_name": "ARMAC LOCAÇÃO, LOGÍSTICA E SERVIÇOS S.A",
+        "aliases": ["armac"]
+    },
+    {
+        "canonical_name": "ATACADÃO S.A",
+        "aliases": ["atacadao"]
+    },
+    {
+        "canonical_name": "AUREN ENERGIA S.A",
+        "aliases": ["auren", "auren energia"]
+    },
+    {
+        "canonical_name": "AZUL S.A",
+        "aliases": ["azul", "azul linhas aereas"]
+    },
+    {
+        "canonical_name": "B3 S.A. - BRASIL, BOLSA, BALCÃO",
+        "aliases": ["b3", "brasil bolsa balcao", "bolsa de valores"] # Descrição funcional como alias
+    },
+    {
+        "canonical_name": "BANCO INTER S.A",
+        "aliases": ["banco inter", "inter"]
+    },
+    {
+        "canonical_name": "BCO SANTANDER (BRASIL) S.A",
+        "aliases": ["santander", "banco santander"]
+    },
+    {
+        "canonical_name": "BR MALLS PARTICIPACOES S.A",
+        "aliases": ["br malls", "brmalls"] # Agora parte da Allos
+    },
+    {
+        "canonical_name": "BRASKEM S.A",
+        "aliases": ["braskem"]
+    },
+    {
+        "canonical_name": "BRF S.A",
+        "aliases": ["brf", "brasil foods", "sadia", "perdigao"] # Marcas importantes
+    },
+    {
+        "canonical_name": "CAMIL ALIMENTOS S.A",
+        "aliases": ["camil", "camil alimentos"]
+    },
+    {
+        "canonical_name": "CCR S.A",
+        "aliases": ["ccr"]
+    },
+    {
+        "canonical_name": "CEA MODAS S.A",
+        "aliases": ["cea", "cea modas"]
+    },
+    {
+        "canonical_name": "CENTRAIS ELET BRAS S.A. - ELETROBRAS",
+        "aliases": ["eletrobras", "centrais eletricas brasileiras"]
+    },
+    {
+        "canonical_name": "CIA BRASILEIRA DE DISTRIBUICAO",
+        "aliases": ["cia brasileira de distribuicao", "gpa", "grupo pao de acucar", "pao de acucar"]
+    },
+    {
+        "canonical_name": "CIA HERING",
+        "aliases": ["hering", "cia hering"]
+    },
+    {
+        "canonical_name": "CIA PARANAENSE DE ENERGIA - COPEL",
+        "aliases": ["copel", "cia paranaense de energia"]
+    },
+    {
+        "canonical_name": "CIELO S.A. - INSTITUIÇÃO DE PAGAMENTO",
+        "aliases": ["cielo"]
+    },
+    {
+        "canonical_name": "COGNA EDUCAÇÃO S.A",
+        "aliases": ["cogna", "cogna educacao"]
+    },
+    {
+        "canonical_name": "CONSTRUTORA TENDA S.A",
+        "aliases": ["tenda", "construtora tenda"]
+    },
+    {
+        "canonical_name": "COSAN S.A",
+        "aliases": ["cosan"]
+    },
+    {
+        "canonical_name": "CVC BRASIL OPERADORA E AGÊNCIA DE VIAGENS S.A",
+        "aliases": ["cvc", "cvc brasil"]
+    },
+    {
+        "canonical_name": "DEXCO S.A",
+        "aliases": ["dexco", "duratex"] # Nome antigo é um alias fundamental
+    },
+    {
+        "canonical_name": "DIAGNOSTICOS DA AMERICA S.A",
+        "aliases": ["dasa", "diagnosticos da america"]
+    },
+    {
+        "canonical_name": "DIRECIONAL ENGENHARIA S.A",
+        "aliases": ["direcional", "direcional engenharia"]
+    },
+    {
+        "canonical_name": "EDP - ENERGIAS DO BRASIL S.A",
+        "aliases": ["edp", "energias do brasil"]
+    },
+    {
+        "canonical_name": "EMBRAER S.A.", # Adicionando Embraer como exemplo de empresa famosa
+        "aliases": ["embraer"]
+    },
+    {
+        "canonical_name": "EMPREENDIMENTOS PAGUE MENOS S.A",
+        "aliases": ["pague menos", "farmacias pague menos"]
+    },
+    {
+        "canonical_name": "ENAUTA PARTICIPAÇÕES S.A",
+        "aliases": ["enauta"]
+    },
+    {
+        "canonical_name": "ENERGISA S.A",
+        "aliases": ["energisa"]
+    },
+    {
+        "canonical_name": "ENEVA S.A",
+        "aliases": ["eneva"]
+    },
+    {
+        "canonical_name": "EQUATORIAL S.A",
+        "aliases": ["equatorial", "equatorial energia"]
+    },
+    {
+        "canonical_name": "FLEURY S.A",
+        "aliases": ["fleury", "grupo fleury"]
+    },
+    {
+        "canonical_name": "GAFISA S.A",
+        "aliases": ["gafisa"]
+    },
+    {
+        "canonical_name": "GERDAU S.A",
+        "aliases": ["gerdau"]
+    },
+    {
+        "canonical_name": "GOL LINHAS AEREAS INTELIGENTES S.A",
+        "aliases": ["gol", "gol linhas aereas"]
+    },
+    {
+        "canonical_name": "GRENDENE S.A",
+        "aliases": ["grendene", "melissa", "ipanema", "rider"] # Marcas
+    },
+    {
+        "canonical_name": "GRUPO CASAS BAHIA S.A",
+        "aliases": ["casas bahia", "grupo casas bahia", "via", "via varejo"] # Nomes antigos
+    },
+    {
+        "canonical_name": "GRUPO DE MODA SOMA S.A",
+        "aliases": ["soma", "grupo soma", "animale", "farm"] # Marcas
+    },
+    {
+        "canonical_name": "GRUPO MATEUS S.A",
+        "aliases": ["grupo mateus", "mateus"]
+    },
+    {
+        "canonical_name": "GRUPO SBF S.A",
+        "aliases": ["grupo sbf", "centauro", "nike do brasil"] # Operação da Nike
+    },
+    {
+        "canonical_name": "GUARARAPES CONFECCOES S.A",
+        "aliases": ["guararapes", "riachuelo", "lojas riachuelo"] # Marca principal
+    },
+    {
+        "canonical_name": "HAPVIDA PARTICIPACOES E INVESTIMENTOS S.A",
+        "aliases": ["hapvida", "notre dame intermedica", "gndi"] # Incluindo a empresa fundida
+    },
+    {
+        "canonical_name": "HYPERA S.A",
+        "aliases": ["hypera", "hypermarcas", "neo quimica", "buscopan"] # Nome antigo e marcas
+    },
+    {
+        "canonical_name": "IRB - BRASIL RESSEGUROS S.A",
+        "aliases": ["irb", "irb brasil re"]
+    },
+    {
+        "canonical_name": "ITAU UNIBANCO HOLDING S.A",
+        "aliases": ["itau unibanco", "itau"]
+    },
+    {
+        "canonical_name": "ITAUSA S.A",
+        "aliases": ["itausa"]
+    },
+    {
+        "canonical_name": "JBS S.A.", # Adicionando JBS como exemplo
+        "aliases": ["jbs", "friboi", "seara"]
+    },
+    {
+        "canonical_name": "JHSF PARTICIPACOES S.A",
+        "aliases": ["jhsf"]
+    },
+    {
+        "canonical_name": "JSL S.A",
+        "aliases": ["jsl"]
+    },
+    {
+        "canonical_name": "LOCALIZA RENT A CAR S.A",
+        "aliases": ["localiza", "localiza rent a car"]
+    },
+    {
+        "canonical_name": "LOJAS RENNER S.A",
+        "aliases": ["renner", "lojas renner"]
+    },
+    {
+        "canonical_name": "LWSA S.A",
+        "aliases": ["lwsa", "locaweb"] # Nome antigo
+    },
+    {
+        "canonical_name": "M.DIAS BRANCO S.A. IND COM DE ALIMENTOS",
+        "aliases": ["m dias branco", "mdias branco"]
+    },
+    {
+        "canonical_name": "MAGAZINE LUIZA S.A",
+        "aliases": ["magazine luiza", "magalu"] # Apelido popular é essencial
+    },
+    {
+        "canonical_name": "MARCOPOLO S.A",
+        "aliases": ["marcopolo"]
+    },
+    {
+        "canonical_name": "MARFRIG GLOBAL FOODS S.A.", # Adicionando Marfrig como exemplo
+        "aliases": ["marfrig"]
+    },
+    {
+        "canonical_name": "MINERVA S.A",
+        "aliases": ["minerva", "minerva foods"]
+    },
+    {
+        "canonical_name": "MOVIDA PARTICIPACOES S.A",
+        "aliases": ["movida"]
+    },
+    {
+        "canonical_name": "MRV ENGENHARIA E PARTICIPACOES S.A",
+        "aliases": ["mrv", "mrv engenharia"]
+    },
+    {
+        "canonical_name": "MULTIPLAN - EMPREEND IMOBILIARIOS S.A",
+        "aliases": ["multiplan"]
+    },
+    {
+        "canonical_name": "NATURA &CO HOLDING S.A",
+        "aliases": ["natura &co", "natura", "avon", "the body shop"] # Marcas do grupo
+    },
+    {
+        "canonical_name": "NU HOLDINGS LTD",
+        "aliases": ["nu holdings", "nubank"] # Marca principal
+    },
+    {
+        "canonical_name": "ODONTOPREV S.A",
+        "aliases": ["odontoprev"]
+    },
+    {
+        "canonical_name": "PÃO DE AÇÚCAR - CIA BRASILEIRA DE DISTRIBUIÇÃO", # Forma alternativa de referenciar GPA
+        "aliases": ["pao de acucar", "gpa"]
+    },
+    {
+        "canonical_name": "PETROBRAS - PETRÓLEO BRASILEIRO S.A.", # Adicionando Petrobras como exemplo
+        "aliases": ["petrobras", "petroleo brasileiro"]
+    },
+    {
+        "canonical_name": "PETRORECÔNCAVO S.A",
+        "aliases": ["petroreconcavo"]
+    },
+    {
+        "canonical_name": "PORTO SEGURO S.A",
+        "aliases": ["porto seguro", "porto"] # Nome curto
+    },
+    {
+        "canonical_name": "PRIO S.A",
+        "aliases": ["prio", "petrorio"] # Nome antigo
+    },
+    {
+        "canonical_name": "QUALICORP CONSULTORIA E CORRETORA DE SEGUROS S.A",
+        "aliases": ["qualicorp"]
+    },
+    {
+        "canonical_name": "RAIA DROGASIL S.A",
+        "aliases": ["raia drogasil", "drogasil", "droga raia", "rd"]
+    },
+    {
+        "canonical_name": "RAIZEN S.A",
+        "aliases": ["raizen"]
+    },
+    {
+        "canonical_name": "REDE DOR SÃO LUIZ S.A",
+        "aliases": ["rede dor", "rede d'or"]
+    },
+    {
+        "canonical_name": "RUMO S.A",
+        "aliases": ["rumo", "rumo logistica"]
+    },
+    {
+        "canonical_name": "SABESP - CIA DE SANEAMENTO BÁSICO DO ESTADO DE SÃO PAULO", # Adicionando Sabesp como exemplo
+        "aliases": ["sabesp", "cia de saneamento basico de sao paulo"]
+    },
+    {
+        "canonical_name": "SENDAS DISTRIBUIDORA S.A.", # Adicionando Assaí como exemplo
+        "aliases": ["sendas distribuidora", "assai", "assai atacadista"]
+    },
+    {
+        "canonical_name": "SIMPAR S.A",
+        "aliases": ["simpar"]
+    },
+    {
+        "canonical_name": "SLC AGRICOLA S.A",
+        "aliases": ["slc", "slc agricola"]
+    },
+    {
+        "canonical_name": "SMARTFIT ESCOLA DE GINÁSTICA E DANÇA S.A",
+        "aliases": ["smartfit"]
+    },
+    {
+        "canonical_name": "SUZANO S.A",
+        "aliases": ["suzano", "suzano papel e celulose"]
+    },
+    {
+        "canonical_name": "TELEFÔNICA BRASIL S.A",
+        "aliases": ["telefonica brasil", "vivo", "telefonica"] # Marca principal
+    },
+    {
+        "canonical_name": "TIM S.A",
+        "aliases": ["tim"]
+    },
+    {
+        "canonical_name": "TOTVS S.A",
+        "aliases": ["totvs"]
+    },
+    {
+        "canonical_name": "ULTRAPAR PARTICIPACOES S.A",
+        "aliases": ["ultrapar", "grupo ultra", "ipiranga", "ultragaz"] # Marcas
+    },
+    {
+        "canonical_name": "VALE S.A",
+        "aliases": ["vale", "cia vale do rio doce"] # Nome antigo
+    },
+    {
+        "canonical_name": "VAMOS LOCAÇÃO DE CAMINHÕES, MÁQUINAS E EQUIP. S.A",
+        "aliases": ["vamos", "vamos locacao"]
+    },
+    {
+        "canonical_name": "VIBRA ENERGIA S.A",
+        "aliases": ["vibra", "vibra energia", "br distribuidora"] # Nome antigo
+    },
+    {
+        "canonical_name": "VIVARA PARTICIPAÇOES S.A",
+        "aliases": ["vivara"]
+    },
+    {
+        "canonical_name": "VULCABRAS S.A",
+        "aliases": ["vulcabras", "olympikus", "under armour brasil"] # Marcas
+    },
+    {
+        "canonical_name": "WEG S.A",
+        "aliases": ["weg"]
+    },
+    {
+        "canonical_name": "YDUQS PARTICIPACOES S.A",
+        "aliases": ["yduqs", "estacio"] # Marca principal
+    },
+    {
+        "canonical_name": "ZAMP S.A",
+        "aliases": ["zamp", "burger king brasil", "bk brasil"] # Operadora
+    }
+]
+
+# Nota: Esta lista foca nas empresas mais conhecidas da sua base original.
+# Para um sistema completo, o ideal é aplicar essa lógica para todas as empresas da lista,
+# preenchendo os aliases para cada uma.
