932562e22545c6edb53ff4de5e66fdabe1415d7a
Danilo da Cruz Ferraz <93060216+tovarich86@users.noreply.github.com>
2025-07-21 15:06:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -854,7 +854,7 @@ def main():
                             protocolo_match = re.search(r'NumeroProtocoloEntrega=(\d+)', url)
                             protocolo = protocolo_match.group(1) if protocolo_match else "N/A"
                             st.markdown(f"**{display_text}** (Protocolo: **{protocolo}**)")
-                            st.markdown(f"↳ [Link Direto]({url}) | [Buscar na CVM]({CVM_SEARCH_URL})", unsafe_allow_html=True)
+                            st.markdown(f"↳ [Link Direto]({url}) )", unsafe_allow_html=True)
             
                         elif "frmExibirArquivoFRE" in url:
                             # Este bloco não usa a variável 'protocolo'
