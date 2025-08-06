87eb5609bd2e7359128e9caacbb588434269ab0d
Danilo da Cruz Ferraz <93060216+tovarich86@users.noreply.github.com>
2025-08-06 12:50:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -104,9 +104,9 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 MODEL_NAME = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
 TOP_K_SEARCH = 5
 TOP_K_INITIAL_RETRIEVAL = 30
-TOP_K_FINAL = 16
+TOP_K_FINAL = 15
 GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY", "")
-GEMINI_MODEL = "gemini-2.0-flash-lite" # Recomendo usar um modelo mais recente se possível
+GEMINI_MODEL = "gemini-2.0-flash" # Recomendo usar um modelo mais recente se possível
 CVM_SEARCH_URL = "https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx"
 
 FILES_TO_DOWNLOAD = {
