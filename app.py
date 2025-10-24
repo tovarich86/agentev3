55770e3852d81ac033b46e2cd577c71c53c1eba4
Danilo da Cruz Ferraz <93060216+tovarich86@users.noreply.github.com>
2025-10-24 15:20:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ TOP_K_SEARCH = 5
 TOP_K_INITIAL_RETRIEVAL = 30
 TOP_K_FINAL = 15
 GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY", "")
-GEMINI_MODEL = "gemini-2.0-flash" # Recomendo usar um modelo mais recente se possível
+GEMINI_MODEL = "gemini-2.5-flash-lite" # Recomendo usar um modelo mais recente se possível
 CVM_SEARCH_URL = "https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx"
 
 FILES_TO_DOWNLOAD = {
